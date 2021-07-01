8d1f4dc0954a30be507af5a0d8ea6855d27220d1
yogesh-karki <yogesh@hueshine.com>
2021-07-02 00:45:52 +0545

    [UPDATE] map_fly_to

--- js/script.js
+++ js/script.js
@@ -1,32 +1,38 @@
-// import {districts,provinces} from './resources.js';
+import {districts,provinces} from '../resources.js';
 
 
-// provinces.forEach((item) => {
-//     $("#provinces").append(`<option value="${item.id}">${item.title}</option>`)
-// });
+provinces.forEach((item) => {
+    $("#provinces").append(`<option value="${item.id}">${item.title}</option>`)
+});
 
-// $('#provinces').on('change',function(){
-//     var provinceID = $(this).val();
-//     $('#districts').html("");
-//     $('#districts').siblings('.nice-select').remove()
-//     var items = districts.filter((item)=>{
-//         return item.province==provinceID;
-//     });
-//     $("#districts").append(`<option value="-1" selected disabled>Select</option>`)
-//     items.forEach((item) => {
-//         $("#districts").append(`<option value="${item.id}">${item.title}</option>`)
-//     });
-//     $('select').niceSelect();
-// });
+$('#provinces').on('change',function(){
+    var provinceID = $(this).val();
+    $('#districts').html("");
+    $('#districts').siblings('.nice-select').remove()
+    var items = districts.filter((item)=>{
+        return item.province==provinceID;
+    });
+    $("#districts").append(`<option value="-1" selected disabled>Select</option>`)
+    items.forEach((item) => {
+        $("#districts").append(`<option value="${item.id}">${item.title}</option>`)
+    });
+    $('select').niceSelect();
+});
 
-// $('.update').on('click',(e) => {
-//     e.preventDefault();
-//     var selectedProvince = $('#provinces').val();
-//     var selectedDistrict = $('#districts').val();
-//     console.log(selectedProvince, selectedDistrict)
+$('.update').on('click',(e) => {
+    e.preventDefault();
+    var selectedProvince = $('#provinces').val();
+    var selectedDistrict = $('#districts').val();
+    console.log(selectedProvince, selectedDistrict)
+
+    map.flyTo({
+        center: [85.4679, 28.050],
+        zoom: 7,
+        essential: true // this animation is considered essential with respect to prefers-reduced-motion
+    });
 
 
-// });
+});
 
 $('select').niceSelect();
 
@@ -37,7 +43,7 @@ var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/yogeshkarki/ckqhmewto4m2317p8n8qarotc",
     center: [83.0074, 28.4764],
-    zoom: 6.6,
+    zoom: 6,
   
 });
 
@@ -94,18 +100,129 @@ map.on("load", function () {
         },
     });
 
+    // map.addLayer({
+        
+
+    //     id: "unclustered-point",
+    //     type: "circle",
+    //     source: "cylinders",
+    //     filter: ["!", ["has", "point_count"]],
+    //     paint: {
+    //         "circle-color": "#11b4da",
+    //         "circle-radius": 10,
+    //         "circle-stroke-width": 1,
+    //         "circle-stroke-color": "#fff",
+    //     },
+    // });
+
     map.addLayer({
-        id: "unclustered-point",
-        type: "circle",
+        'id': 'unclustered-point',
+        'type': 'symbol',
         source: "cylinders",
         filter: ["!", ["has", "point_count"]],
-        paint: {
-            "circle-color": "#11b4da",
-            "circle-radius": 10,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#fff",
+        'layout': {
+        'icon-image': 'pulsing-dot'
+        }
+        });
+
+    map.addSource('dot-point', {
+        'type': 'geojson',
+        'data': {
+        'type': 'FeatureCollection',
+        'features': [
+        {
+        'type': 'Feature',
+        'geometry': {
+        'type': 'Point',
+        'coordinates': [83.0074, 28.4764] // icon position [lng, lat]
+        }
+        }
+        ]
+        }
+        });
+
+        
+
+    var size = 100;
+
+    var pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+         
+        // When the layer is added to the map,
+        // get the rendering context for the map canvas.
+        onAdd: function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
         },
-    });
+         
+        // Call once before every frame where the icon will be used.
+        render: function () {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+         
+        var radius = (size / 2) * 0.3;
+        var outerRadius = (size / 2) * 0.4 * t + radius;
+        var context = this.context;
+         
+        // Draw the outer circle.
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+        this.width / 2,
+        this.height / 2,
+        outerRadius,
+        0,
+        Math.PI * 2
+        );
+        context.fillStyle = 'rgba(195, 234, 243,' + (1 - t) + ')';
+        context.fill();
+         
+        // Draw the inner circle.
+        context.beginPath();
+        context.arc(
+        this.width / 2,
+        this.height / 2,
+        radius,
+        0,
+        Math.PI * 2
+        );
+        context.fillStyle = '#11b4da';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+         
+        // Update this image's data with data from the canvas.
+        this.data = context.getImageData(
+        0,
+        0,
+        this.width,
+        this.height
+        ).data;
+         
+        // Continuously repaint the map, resulting
+        // in the smooth animation of the dot.
+        map.triggerRepaint();
+         
+        // Return `true` to let the map know that the image was updated.
+        return true;
+        }
+        };
+
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+        
+
+        
+
+
+
+
+
 
     // inspect a cluster on click
     map.on("click", "clusters", function (e) {
